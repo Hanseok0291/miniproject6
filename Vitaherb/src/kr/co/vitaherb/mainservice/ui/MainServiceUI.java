1e720c925b579a1aafed947a4f887aaafeb2568f
USER-PC\USER <>
2018-03-14 19:13:06 +0900

    MainServiceUI 작성

--- Vitaherb/src/kr/co/vitaherb/mainservice/ui/MainServiceUI.java
+++ Vitaherb/src/kr/co/vitaherb/mainservice/ui/MainServiceUI.java
@@ -6,16 +6,28 @@ import org.apache.ibatis.session.SqlSession;
 
 import common.db.BaseUI;
 import common.db.MyAppSqlConfig;
-import kr.co.vitaherb.login.ui.FindUserUI;
-import kr.co.vitaherb.login.ui.LoginUI;
+import kr.co.vitaherb.cart.ui.CartUI;
+import kr.co.vitaherb.detailgoodsinfo.ui.DetailGoodsInfoUI;
+import kr.co.vitaherb.goods.ui.HealthSupplementUI;
+import kr.co.vitaherb.goods.ui.PetSupplementUI;
+import kr.co.vitaherb.goods.ui.SuperFoodUI;
+import kr.co.vitaherb.mapper.CartMapper;
+import kr.co.vitaherb.mapper.GoodsMapper;
+import kr.co.vitaherb.mapper.OrderMapper;
+import kr.co.vitaherb.mapper.ReviewMapper;
 import kr.co.vitaherb.mapper.UserMapper;
-import kr.co.vitaherb.signup.ui.SignUpUI;
+import kr.co.vitaherb.orderhistory.ui.OrderHistoryUI;
 
 public class MainServiceUI {
+	
 	private Scanner sc = new Scanner(System.in);
+    private GoodsMapper goodsmap;
+    private CartMapper cartmap;
+    private OrderMapper ordermap;
+    private ReviewMapper reviewmap;
     private UserMapper usermap;
     
-	public LoginViewUI() {
+	public MainServiceUI() {
 		SqlSession session = MyAppSqlConfig.getSqlSession();
 		usermap = session.getMapper(UserMapper.class);
 	}
@@ -25,9 +37,12 @@ public class MainServiceUI {
 		while(true) {
 			BaseUI ui = null;
 			switch(menu()) {
-			case 1 : ui = new SignUpUI(usermap); break;
-			case 2 : ui = new LoginUI(usermap); break;
-			case 3 : ui = new FindUserUI(usermap); break;
+			case 1 : ui = new HealthSupplementUI(goodsmap); break;
+			case 2 : ui = new SuperFoodUI(goodsmap); break;
+			case 3 : ui = new PetSupplementUI(goodsmap); break;
+			case 4 : ui = new DetailGoodsInfoUI(goodsmap, reviewmap); break;
+			case 5 : ui = new CartUI(goodsmap); break;
+			case 6 : ui = new OrderHistoryUI(ordermap, goodsmap); break;
 			}
 			ui.service();
 		}
@@ -38,11 +53,14 @@ public class MainServiceUI {
 	  
 	  System.out.println("Vitaherb에 오신 것을 환영합니다.");
 	  System.out.println("************************");
-	  System.out.println("1. 회원가입");
-	  System.out.println("2. 로그인");
-	  System.out.println("3. 아이디, 비밀번호 찾기");
+	  System.out.println("1. 건강보조식품");
+	  System.out.println("2. 슈퍼푸드");
+	  System.out.println("3. 반려동물영양제");
+	  System.out.println("4. 상품 검색");
+	  System.out.println("5. 장바구니");
+	  System.out.println("6. 주문조회");
 	  System.out.println("************************");
-	  System.out.print("원하시는 기능을 선택하세요 : ");
+	  System.out.print("조회하고싶은 항목을 선택하세요 : ");
 	  return Integer.parseInt(sc.nextLine());
 	  
   }
