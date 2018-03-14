0c339cc10a464ad84608d785bfc58f601139bf07
Hanseok0291 <lbukji@naver.com>
2018-03-14 19:17:19 +0900

    SignUpUI 설정

--- Vitaherb/src/kr/co/vitaherb/signup/ui/SignUpUI.java
+++ Vitaherb/src/kr/co/vitaherb/signup/ui/SignUpUI.java
@@ -1,5 +1,19 @@
 package kr.co.vitaherb.signup.ui;
 
-public class SignUpUI {
+import common.db.BaseUI;
+import kr.co.vitaherb.mapper.UserMapper;
 
+public class SignUpUI extends BaseUI {
+	private UserMapper dao;
+	
+	public SignUpUI(UserMapper dao) {
+		this.dao = dao;
+	}
+	
+	public void service() {
+		UserMapper user = new UserMapper();
+		System.out.println("아래 정보를 작성해 주세요");
+		System.out.print("이름 : ");
+		
+	}
 }
