a9b9f800c90160686c0b92e136c0d209fe9e3a93
Hanseok0291 <lbukji@naver.com>
2018-03-15 11:25:53 +0900

    usermapper, sinupUI 설정

--- Vitaherb/src/kr/co/vitaherb/mapper/UserMapper.java
+++ Vitaherb/src/kr/co/vitaherb/mapper/UserMapper.java
@@ -1,5 +1,8 @@
 package kr.co.vitaherb.mapper;
 
 public class UserMapper {
+import kr.co.vitaherb.domain.User;
 
+public interface UserMapper {
+    void insertUser(User user);
 }
--- Vitaherb/src/kr/co/vitaherb/signup/ui/SignUpUI.java
+++ Vitaherb/src/kr/co/vitaherb/signup/ui/SignUpUI.java
@@ -1,19 +1,32 @@
 package kr.co.vitaherb.signup.ui;
 
 import common.db.BaseUI;
+import kr.co.vitaherb.domain.User;
 import kr.co.vitaherb.mapper.UserMapper;
 
 public class SignUpUI extends BaseUI {
-	private UserMapper dao;
-	
-	public SignUpUI(UserMapper dao) {
-		this.dao = dao;
-	}
-	
-	public void service() {
-		UserMapper user = new UserMapper();
-		System.out.println("아래 정보를 작성해 주세요");
-		System.out.print("이름 : ");
-		
-	}
+    private UserMapper dao;
+    private UserMapper um;
+    
+    public SignUpUI(UserMapper dao) {
+        this.dao = dao;
+    public SignUpUI(UserMapper um) {
+        this.um = um;
+    }
+    
+    public void service() {
+        UserMapper user = new UserMapper();
+        System.out.println("아래 정보를 작성해 주세요");
+        System.out.print("이름 : ");
+        User user = new User();
+        user.setUserName(getStr("이름 : "));
+        user.setUserBirth(getInt("생년월일 : "));
+        user.setUserGender(getStr("성별 : "));
+        user.setUserId(getStr("아이디 : "));
+        user.setUserPassword(getStr("비밀번호 : "));
+        user.setUserEmail(getStr("이메일 : "));
+        user.setUserAddr(getStr("주소 : "));
+        user.setUserPhone(getStr("연락처 : "));
+        
+    }
 }
